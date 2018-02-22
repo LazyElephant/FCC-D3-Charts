1ee59b96658f53282776899f8d0e4c03361639bf
Tim Schmidt <tschmidt@lazyelephantstudio.com>
2018-02-22 18:03:11 -0500

    Finish Heat Map

--- static/projects/HeatMap.js
+++ static/projects/HeatMap.js
@@ -1,177 +1,185 @@
-/*
-Objective: Build a CodePen.io app that is functionally similar to this: https://codepen.io/FreeCodeCamp/full/aNLYPp/.
+function HeatMap(data) {
 
-Fulfill the below user stories. Use whichever libraries or APIs you need. Give it your own personal style.
-
-User Story: I can view a heat map with data represented both on the Y and X axis.
-
-User Story: Each cell is colored based its relationship to other data.
-
-User Story: I can mouse over a cell in the heat map to get more exact information.
-
-Hint: Here's a dataset you can use to build this: https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/global-temperature.json
-
-Remember to use Read-Search-Ask if you get stuck.
-
-When you are finished, click the "I've completed this challenge" button and include a link to your CodePen.
-
-You can get feedback on your project by sharing it with your friends on Facebook.
-*/
-
-function HeatMap(data, options) {
-  // define useful constants
+  // layout constants
   const VIEWBOX_WIDTH = 1000
   const VIEWBOX_HEIGHT = 600
   const MARGIN = {
-      LEFT: 100,
-      RIGHT: 100,
-      TOP: 100,
-      BOTTOM: 100,
-    }
+        LEFT: 150,
+        RIGHT: 50,
+        TOP: 150,
+        BOTTOM: 100,}
   const CHART_WIDTH = VIEWBOX_WIDTH - MARGIN.LEFT - MARGIN.RIGHT
   const CHART_HEIGHT = VIEWBOX_HEIGHT - MARGIN.TOP - MARGIN.BOTTOM
+  
   const NUM_SAMPLES = data.length
+  const BAR_WIDTH = Math.ceil(CHART_WIDTH / NUM_SAMPLES)
+  const BAR_HEIGHT = Math.ceil(CHART_HEIGHT / 12)
+
+  // styling constants
   const LABEL_FONT_SIZE = '25px'
   const TITLE_FONT_SIZE = '40px'
   const TICK_FONT_SIZE = '16px'
+  
+  // labels
+  const TITLE = 'Monthly Global Land Surface Temperature in °C'
+  const SUBTITLE = 'from 1753 to 2015'
+  const X_LABEL = 'Years'
+  const Y_LABEL = 'Month'
+  const MONTHS = [
+    'January', 
+    'February', 
+    'March', 
+    'April', 
+    'May', 
+    'June', 
+    'July', 
+    'August', 
+    'September', 
+    'October', 
+    'November', 
+    'December'
+  ]
+
+  // data constants
+  const [MIN_TEMP, MAX_TEMP] = d3.extent(data, d => d.temperature)
+  const [MIN_YEAR, MAX_YEAR] = d3.extent(data, d => d.year)
+
+  // scales
+  const xScale = d3.scaleLinear().domain([MIN_YEAR, MAX_YEAR]).range([0, CHART_WIDTH])
+  const yScale = d3.scaleLinear().domain([1, 13]).range([0, CHART_HEIGHT])
+  const colorScale = d3.scaleSequential(d3.interpolateRdYlBu).domain([MAX_TEMP, MIN_TEMP])
+  
+  const legendColors = d3.range(0, MAX_TEMP).map(colorScale)
 
   const svg = d3.select('body')
     .append('div')
-      .attr('class', 'container')
+    .attr('class', 'container')
     .append('svg')
-      .attr('height', '100%')
-      .attr('width', '100%')
-      .attr('viewBox', `0 0 ${VIEWBOX_WIDTH} ${VIEWBOX_HEIGHT}`)
-      .attr('preserveAspectRatio', 'none')
+    .attr('height', '100%')
+    .attr('width', '100%')
+    .attr('viewBox', `0 0 ${VIEWBOX_WIDTH} ${VIEWBOX_HEIGHT}`)
+    .attr('preserveAspectRatio', 'none')
 
   // Add Title and Subtitle
-  if (options.title) {
-    const chartTitle = svg.append('g')
-      .attr('transform', `translate(${VIEWBOX_WIDTH / 2} ${MARGIN.TOP / 2})`)
-      chartTitle.append('text')
-      .text(options.title)
-      .style('text-anchor', 'middle')
-      .style('font-size', TITLE_FONT_SIZE)
-      
-    if (options.subtitle) {
-      chartTitle.append('text')
-        .text(options.subtitle)
-        .style('text-anchor', 'middle')
-        .style('font-size', LABEL_FONT_SIZE)
-        .attr('transform', `translate(0 40)`)
-        
-    }
-  }
+  const chartTitle = svg.append('g')
+    .attr('transform', `translate(${VIEWBOX_WIDTH / 2} ${MARGIN.TOP / 2})`)
+    chartTitle.append('text')
+    .text(TITLE)
+    .style('text-anchor', 'middle')
+    .style('font-size', TITLE_FONT_SIZE)
+    
+  chartTitle.append('text')
+    .text(SUBTITLE)
+    .style('text-anchor', 'middle')
+    .style('font-size', LABEL_FONT_SIZE)
+    .attr('transform', `translate(0 30)`)
   
   // Add tooltip container
   const tooltip = d3.select('body')
     .append('div')
-      .attr('class', 'tooltip')
-      .style('opacity', 0)
+    .attr('class', 'tooltip')
+    .style('opacity', 0)
 
-  // Add Y Axis
-  const scaleY = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d.Place) + 2])
-    .range([0, CHART_HEIGHT])
-  const yAxis = d3.axisLeft(scaleY)
-
-  svg.append('g')
-      .attr('transform', `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
-      .classed('y-axis', true)
-    .call(yAxis)
+  // Add month labels
+  const monthTicks = svg.append('g')
+    .attr('transform', `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
     .selectAll('text')
-      .attr('transform', 'translate(-5 0)')
-      .attr('font-size', TICK_FONT_SIZE)
-  
-  if (options.labelY) {
-    svg.append('text')
-      .text(options.labelY)
-      .attr('text-anchor', 'middle')
-      .attr('transform', `translate(${MARGIN.LEFT / 2} ${VIEWBOX_HEIGHT / 2}) rotate(-90)`)
-      .style('font-size', LABEL_FONT_SIZE)
-  }
-
-  // Add X axis
-  const scaleX = d3.scaleLinear()
-    .domain([d3.max(data, d => d.Seconds) + 40, 0])
-    .range([0, CHART_WIDTH])
-  const xAxis = d3.axisBottom(scaleX)
-  xAxis.tickFormat( seconds => `${Math.floor(seconds/60)}:${seconds%60}`)
-
-  svg.append('g')
-      .attr('transform', `translate(${MARGIN.LEFT}, ${VIEWBOX_HEIGHT - MARGIN.BOTTOM})`)
-      .classed('x-axis', true)
-    .call(xAxis)
-    .selectAll('text')
-      .attr('transform', 'translate(0 5)')
-      .attr('font-size', TICK_FONT_SIZE)
-
-  if (options.labelX) {
-    svg.append('text')
-      .text(options.labelX)
-      .attr('text-anchor', 'middle')
-      .attr('dominant-baseline', 'hanging')
-      .attr('transform', `translate(${VIEWBOX_WIDTH / 2} ${VIEWBOX_HEIGHT - (MARGIN.BOTTOM / 2)})`)
-      .style('font-size', LABEL_FONT_SIZE)
-  }
-
-  // Add <g> tags to act as anchors for data points
-  const points = svg.append('g')
-      .attr('id', 'points')
-      .attr('transform', `translate(${MARGIN.LEFT} ${MARGIN.TOP})`)
-    .selectAll('g')
     .data(data)
     .enter()
+    .append('text')
+    .text(d => MONTHS[d.month - 1])
+    .attr('text-anchor', 'end')
+    .attr('font-size', TICK_FONT_SIZE)
+    .attr('transform', d => `translate(-5 ${(yScale(d.month)) + 20})`)
+  
+  svg.append('text')
+    .text(Y_LABEL)
+    .attr('text-anchor', 'middle')
+    .attr('transform', `translate(${MARGIN.LEFT / 4} ${VIEWBOX_HEIGHT / 2}) rotate(-90)`)
+    .style('font-size', LABEL_FONT_SIZE)
+
+  // Add X axis
+  const xAxis = d3.axisBottom(xScale)
+    .tickFormat(year => `${year}`)
+
+  svg.append('g')
+    .attr('transform', `translate(${MARGIN.LEFT}, ${VIEWBOX_HEIGHT - MARGIN.BOTTOM})`)
+    .classed('x-axis', true)
+    .call(xAxis)
+    .selectAll('text')
+    .attr('transform', 'translate(0 5)')
+    .attr('font-size', TICK_FONT_SIZE)
+
+  svg.append('text')
+    .text(X_LABEL)
+    .attr('text-anchor', 'middle')
+    .attr('dominant-baseline', 'hanging')
+    .attr('transform', `translate(${VIEWBOX_WIDTH / 2} ${VIEWBOX_HEIGHT - (MARGIN.BOTTOM / 2)})`)
+    .style('font-size', LABEL_FONT_SIZE)
+  
+  // Add <g> tags to act as anchors for data points
+  const rects = svg.append('g')
+    .attr('id', 'points')
+    .attr('transform', `translate(${MARGIN.LEFT} ${MARGIN.TOP})`)
+    .selectAll('rect')
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr('x', d => xScale(d.year))
+    .attr('y', d => yScale(d.month))
+    .attr('width', BAR_WIDTH)
+    .attr('height', BAR_HEIGHT)
+    .attr('fill', d => colorScale(d.temperature))
+    .attr('stroke', d => colorScale(d.temperature))
+    .on('mouseover', (d) => {
+      tooltip.transition()
+        .duration(150)
+        .style('opacity', .9)
+      tooltip.html(
+        `<span>${MONTHS[d.month - 1]}, ${d.year}</span><br/>
+        <span>Temp: ${d.temperature.toFixed(3)}°C</span><br/>
+        <span>Variance: ${d.variance.toFixed(3)}</span>`
+      )	
+      .style('left', `${d3.event.pageX }px`)
+      .style('top', `${d3.event.pageY - 100}px`)
+    })
+    .on('mouseout', function(d) {		
+      tooltip.transition()		
+          .duration(100)		
+          .style('opacity', 0)
+    })  
+
+    const legend = svg.append('g')
+      .attr('transform', `translate(${VIEWBOX_WIDTH - 400} ${VIEWBOX_HEIGHT - 50})`)
+      .attr('class', 'legent')
+      .selectAll('g')
+      .data(legendColors)
+      .enter()
       .append('g')
-        .attr('transform', d => `translate(${scaleX(d.Seconds)} ${scaleY(d.Place)})`)
-        .on('mouseover', (d) => {
-          tooltip.transition()
-            .duration(150)
-            .style('opacity', .9)
-          tooltip.html(
-            `<span>${d.Name}: ${d.Nationality}</span><br/>
-            <span>Year: ${d.Year}</span>
-            <span>Time: ${d.Time}</span><br/><br/>
-            <span>${d.Doping}</span>`
-          )	
-          .style('left', `${d3.event.pageX - 200}px`)
-          .style('top', `${d3.event.pageY - 50}px`)
-        })
-        .on('mouseout', function(d) {		
-          tooltip.transition()		
-              .duration(100)		
-              .style('opacity', 0)
-        })
-        
-  points.append('circle')
-    .attr('cx', 0)
-    .attr('cy', 0)
-    .attr('r', 5)
-    .attr('fill', d => d.Doping ? 'black' : 'white')       
-  points.append('text')
-    .attr('transform', 'translate(15 4)')
-    .text(d => d.Name)
-    .attr('font-size', '12px')
+      .attr('transform', (d, i) => `translate(${i * 20} 0)`)
+
+    legend.append('rect')
+      .attr('height', 20)
+      .attr('width', 20)
+      .attr('fill', d => d)
     
+    legend.append('text')
+      .attr('dominant-baseline', 'hanging')
+      .attr('transform', 'translate(2 20)')
+      .attr('font-size', TICK_FONT_SIZE)
+      .text((d, i) => i)
 }
 
 
 // load the dataset and render the plot
 d3.json('static/data/global-temperature.json', (err, data) => {
-    if (err) {
-      console.error("Error loading the data")
-      return
-    }
+    if (err) throw err
 
-    const data_final = data.map(d => ({
-      ...d,
-      Seconds: d.Seconds - data[0].Seconds
+    const data_final = data.monthlyVariance.map(datum => ({
+      ...datum,
+      temperature: data.baseTemperature + datum.variance
     }))
-    requestAnimationFrame(() => HeatMap(data_final, {
-      title:'Monthly Global Land Surface Temperature',
-      subtitle: "1753 - 2015",
-      labelY: 'Month',
-      labelX: 'Years',
-    }))
+
+    requestAnimationFrame(() => HeatMap(data_final))
   }
 )
