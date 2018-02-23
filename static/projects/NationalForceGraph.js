47ca6960013fb9371f720f1f00c00cd04c579f51
Tim Schmidt <tschmidt@lazyelephantstudio.com>
2018-02-23 01:26:43 -0500

    Make force directed graph

--- static/projects/NationalForceGraph.js
+++ static/projects/NationalForceGraph.js
@@ -1,19 +1,117 @@
-/*
-Objective: Build a CodePen.io app that is functionally similar to this: https://codepen.io/FreeCodeCamp/full/xVopBo.
+function ForceDirectedGraph(data) {
+  const WIDTH = 1000
+  const HEIGHT = 1000
+ 
+  const svg = d3
+    .select('body')
+    .append('div')
+    .attr('class', 'container')
+    .append('svg')
+    .attr('height', HEIGHT)
+    .attr('width', WIDTH)
+  
+ var simulation = d3
+    .forceSimulation()
+    .force("charge", d3.forceManyBody().strength(-100))
+    .force("link", d3.forceLink())
+    .force("x", d3.forceX(WIDTH / 2).strength(0.07))
+    .force('y', d3.forceY(HEIGHT / 2).strength(0.07))
 
-Fulfill the below user stories. Use whichever libraries or APIs you need. Give it your own personal style.
+  var link = svg
+    .append("g")
+    .attr("class", "links")
+    .selectAll("line")
+    .data(data.links)
+    .enter().append("line")
+    .attr('stroke', 'black')
+    .attr("stroke-width", 1)
 
-User Story: I can see a Force-directed Graph that shows which countries share borders.
+  var node = d3
+    .select('.container')
+    .append('div')
+    .attr('class', 'flag-container')
+    .selectAll("img")
+    .data(data.nodes)
+    .enter().append("img")
+    .attr('class', (d) => `flag flag-${d.code}`)
+    .on('mouseover', mouseOver)
+    .on('mouseout', mouseOut)
+    .call(d3.drag()
+      .on('start', dragStart)
+      .on('drag', drag)
+      .on('end', dragEnd))
 
-User Story: I can see each country's flag on its node.
+  simulation
+    .nodes(data.nodes)
+    .on("tick", () => requestAnimationFrame(ticked))
 
-Hint: Here's a dataset you can use to build this: https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json
+  simulation
+    .force('link')
+    .links(data.links)
+    .distance(60)
+    
+  // EVENT HANDLERS
+  function ticked() {
+    link
+        .attr("x1", d => d.source.x)
+        .attr("y1", d => d.source.y)
+        .attr("x2", d => d.target.x)
+        .attr("y2", d => d.target.y)
 
-Hint: You can create a spritesheet of national flags at https://www.flag-sprites.com.
+    node
+        .style("left", d => `${d.x - 10}px`)
+        .style("top", d => `${d.y - 10}px`)
+  }
 
-Remember to use Read-Search-Ask if you get stuck.
+  function mouseOver(d) {
+    tooltip
+      .transition()
+      .duration(150)
+      .style('opacity', .9)
+    tooltip
+      .html(`<p>${d.country}</p>`)	
+      .style('left', `${d3.event.pageX - 50}px`)
+      .style('top', `${d3.event.pageY - 100}px`)
+  }
 
-When you are finished, click the "I've completed this challenge" button and include a link to your CodePen.
+  function mouseOut(d) {		
+    tooltip
+      .transition()		
+      .duration(100)		
+      .style('opacity', 0)
+  }
 
-You can get feedback on your project by sharing it with your friends on Facebook.
-*/
+  function dragStart(d) {
+    if (!d3.event.active) {
+      simulation
+        .alphaTarget(0.3)
+        .restart()
+    }
+    d.fx = d.x
+    d.fy = d.y
+  }
+
+  function drag(d) {
+    d.fx = d3.event.x
+    d.fy = d3.event.y
+  }
+
+  function dragEnd(d) {
+    if (!d3.event.active) {
+      simulation
+        .alphaTarget(0)
+    }
+    d.fx = null
+    d.fy = null
+  }
+} // end ForceDirectedGraph
+
+
+// load the dataset and render the plot
+d3.json('static/data/countries.json', (err, data) => {
+    if (err) throw err
+
+    requestAnimationFrame(() => ForceDirectedGraph(data))
+  }
+)
+
